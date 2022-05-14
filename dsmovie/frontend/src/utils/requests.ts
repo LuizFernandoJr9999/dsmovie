0d2cbc4c7ccb8197748b5733dfd202eb6fff16a0
Luiz Fernando <luizfernandojunior9999@gmail.com>
2022-05-13 22:36:48 -0300

    Teste

--- dsmovie/frontend/src/utils/requests.ts
+++ dsmovie/frontend/src/utils/requests.ts
@@ -1,4 +1,4 @@
-//export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://sp--dsmovie.herokuapp.com";
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
+//export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://sp--dsmovie.herokuapp.com";
 
 
